3075ad0c05451bc87a0cd6e682cb1d81eb5b071e
Patrick <patrickrebecchi@hotmail.com>
2025-04-22 21:52:23 -0300

    Criação e configuração das Migrations

--- UltimanteTeam/Migrations/20250423004923_ConfigurandoTimes.Designer.cs
+++ UltimanteTeam/Migrations/20250423004923_ConfigurandoTimes.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UltimanteTeam.Data;
 
@@ -10,9 +11,11 @@ using UltimanteTeam.Data;
 namespace UltimanteTeam.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250423004923_ConfigurandoTimes")]
+    partial class ConfigurandoTimes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UltimanteTeam/Migrations/20250423004923_ConfigurandoTimes.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UltimanteTeam.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigurandoTimes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Escudo",
+                table: "Times",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Pais",
+                table: "Times",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Escudo",
+                table: "Times");
+
+            migrationBuilder.DropColumn(
+                name: "Pais",
+                table: "Times");
+        }
+    }
+}
