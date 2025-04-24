ecd1b523d5bb5e21da1b96108cde49016639d022
Patrick <patrickrebecchi@hotmail.com>
2025-04-23 21:56:33 -0300

    Criando controller Times

---
+++ UltimanteTeam/Controllers/TimesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UltimanteTeam.Data;
+using UltimanteTeam.Models;
+
+namespace UltimanteTeam.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TimesController : Controller
+{
+    private readonly AppDbContext _context;
+    public TimesController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: Times
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Times>>> GetTimes()
+    {   
+        return await _context.Times.ToListAsync();
+    }
+}
